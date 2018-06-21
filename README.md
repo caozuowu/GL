ef89badd59171e238755f250253eb03ac23be91b
zuowu <zuowu@staff.wei.com>
2018-06-21 16:21:12 +0800

    111

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ### README test code
+
+[连接](https://www.jianshu.com/p/6bda18e953f6)
---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by zuowu on 2018/6/21.
+//  Copyright © 2018年 zuowu. All rights reserved.
+//
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
